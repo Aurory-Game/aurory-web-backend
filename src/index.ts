5580007f1d9ecdb183b38a675372998063a8b282
Levani Tevdoradze <12980455+tevdoradze@users.noreply.github.com>
2022-03-02 22:23:04 +0100

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import '@wrappers/dotenv'
 
 import express from 'express'
+import cors from 'cors'
 import routes from '@routes/index'
 
 import { log } from '@utils/log'
 
 const app = express()
+app.use(cors({
+    origin: 'aurory.io'
+}))
 app.use(express.json())
 
 app.use('/v1', routes)
