12396c001bbd304b3befd73f7742fed9d4f0f6b5
Levani Tevdoradze <12980455+tevdoradze@users.noreply.github.com>
2022-03-02 13:01:31 +0100

    Remove unused packages

--- src/wrappers/dynamoose.ts
+++
@@ -1,9 +0,0 @@
-import * as dynamoose from "dynamoose"
-
-if (process.env.NODE_ENV === 'production') {
-    throw new Error("Production config missing for dynamoose")
-} else {
-    dynamoose.aws.ddb.local(process.env.AWS_ENDPOINT)
-}
-
-export default dynamoose
